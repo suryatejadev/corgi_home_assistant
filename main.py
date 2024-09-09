6ce0b69a7c8ad2d3c78b52875b90d5ac94c526f9
suryatejadev <9112765+suryatejadev@users.noreply.github.com>
2024-09-08 17:47:24 -0700

    added support for phone and mic simultaneous use

--- main.py
+++ main.py
@@ -9,36 +9,22 @@ import sounddevice
 
 
 client = OpenAI()
+PHONE_RECORDINGS_PATH = "./data/easy_voice_recorder"
 
-def get_chatgpt_response(input_path, output_path, transcribe=True):
-    ''' load audio file
-    '''
-    t = time.time()
-    if transcribe:
-        with open(input_path, "rb") as audio_file:
-            transcription = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=audio_file
-            ).text
-    else:
-        transcription = input_path
-    print(f"Transcribed in {time.time() - t:.2f} sec")
-    print(f"Input question: {transcription}")
 
-    '''Generate answer
-    '''
+def get_chatgpt_response(query, output_path):
+    # Generate answer
     t = time.time()
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are an assistant. Always generate short, succint responses, in 1 to 2 lines."},
-        {"role": "user", "content": transcription}
+        {"role": "user", "content": query}
     ]).choices[0].message.content
     print(f"Generated response in {time.time() - t:.2f} sec")
     print(f"ChatGPT Response: {completion}")
 
-    '''Convert answer to speech
-    '''
+    # Convert answer to speech
     t = time.time()
     response = client.audio.speech.create(
         model="tts-1",
@@ -55,91 +41,96 @@ def play_audio(audio_path):
     while mixer.music.get_busy():
         time.sleep(1)
 
+def get_latest_recording():
+    available_files = glob(os.path.join(PHONE_RECORDINGS_PATH, "*.m4a"))
+    if len(available_files) == 0:
+        return
+    latest_recording = sorted(available_files, reverse=True)[0]
+    return latest_recording
+
 def convert_m4a_to_mp3(input_path, output_path):
     return ffmpeg.input(input_path).output(output_path, loglevel="quiet").run()
 
-def get_latest_recording():
-    PHONE_RECORDINGS_PATH = "./data/easy_voice_recorder"
-    old_recording_files = glob(os.path.join(PHONE_RECORDINGS_PATH, "*.m4a"))
-    time.sleep(2)
-    new_recording_files = glob(os.path.join(PHONE_RECORDINGS_PATH, "*.m4a"))
-    diff_files = [k for k in new_recording_files if k not in old_recording_files]
-    if len(diff_files) == 0:
-        return
-    latest_recording = sorted(diff_files, reverse=True)[0]
-    return latest_recording
-
 def get_user_audio_query_from_phone():
+    # check if any new files are added to the phone-synced folder
     latest_recording = get_latest_recording()
-    if latest_recording is None:
-        return None, None
 
+    if latest_recording is None:
+        return
+
+    # acknowledge
     print("Got recording ...")
     play_audio("data/sound_effects/corgi_bark.mp3")
-    recording_name = os.path.basename(latest_recording).split(".")[0]
-    latest_recording_mp3 = f"data/easy_voice_recorder_mp3/{recording_name}.mp3"
+
+    # convert file to mp3
+    latest_recording_mp3 = "tmp_query.mp3"
     t = time.time()
     convert_m4a_to_mp3(
         input_path=latest_recording,
         output_path=latest_recording_mp3
         )
     print(f"Converted to mp3 in {time.time() - t:.2f} sec")
-    return latest_recording_mp3, recording_name
 
-def speech_recognition(hotword=False):
+    # transcribe to get query
+    with open(latest_recording_mp3, "rb") as audio_file:
+            query = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_file
+            ).text
+
+    # cleanup tmp data and remove all files in phone-synced folder
+    os.remove(latest_recording_mp3)
+    for path in glob(os.path.join(PHONE_RECORDINGS_PATH, "*.m4a")):
+        os.remove(path)
+
+    return query
+
+def get_user_audio_query_from_microphone(hotword=False):
     r = sr.Recognizer()
-    while 1:
-        with sr.Microphone() as source:
-            # print("speak")
-            audio = r.listen(source, phrase_time_limit=2 if hotword else 5)
-            # print("heard")
-            said = ""
-            try:
-                t = time.time()
-                said = r.recognize_google(audio).lower()
-                # print(f"you said = {said}, transcribed in {time.time()-t:.2f} sec")
-                if not hotword:
-                    return said
-                if "corgi" in said or "corgee" in said or "corgy" in said or "corgo" in said or "cargo" in said:
-                    play_audio("data/sound_effects/corgi_bark.mp3")
-                    return
-            except Exception as e:
-                pass
-                # print("Exception: " + str(e))
+    with sr.Microphone() as source:
+        audio = r.listen(source, phrase_time_limit=2 if hotword else 5)
+        said = ""
+        try:
+            t = time.time()
+            said = r.recognize_google(audio).lower()
+            if not hotword:
+                return said
+            if "corgi" in said or "corgee" in said or "corgy" in said or "corgo" in said or "cargo" in said:
+                play_audio("data/sound_effects/corgi_bark.mp3")
+                return True
+        except Exception as e:
+            pass
     return
 
-def get_user_audio_query_from_microphone():
-    print('Hotword detection...')
-    speech_recognition(hotword=True)
-    print('Query detection...')
-    return speech_recognition(hotword=False)
-
-def get_user_audio_query(source="phone"):
-    if source == "phone":
-        return get_user_audio_query_from_phone()
-    if source == "microphone":
-        return get_user_audio_query_from_microphone(), None
-    return
+def get_user_audio_query():
+    query_from_phone = get_user_audio_query_from_phone()
+    if query_from_phone is not None:
+        return query_from_phone
+    if get_user_audio_query_from_microphone(hotword=True):
+        return get_user_audio_query_from_microphone(hotword=False)
 
 def run():
     # Get user query
-    user_audio_query, recording_name = get_user_audio_query(source="microphone")
+    user_audio_query = get_user_audio_query()
     if user_audio_query is None:
         return
+    print(f"Input question: {query}")
 
     # Get agent response
-    recording_name = recording_name or "default"
-    output_path = f"data/chatgpt_responses/{recording_name}.mp3"
+    recording_name = "default"
+    output_path = f"agent_reponse.mp3"
     get_chatgpt_response(
         input_path=user_audio_query,
-        output_path=output_path,
-        transcribe=False
+        output_path=output_path
         )
 
     # Play agent response
     t = time.time()
     play_audio(output_path)
     print(f"Played audio in {time.time() - t:.2f} sec")
+
+    # delete agent response
+    os.remove(output_path)
 
 if __name__ == "__main__":
     while 1:
